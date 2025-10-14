ef43971fbd574fdf6e34ee3679673524eb777728
ayumiyua21 <misbhakulridho@gmail.com>
2025-10-14 11:04:29 +0800

    add simple greet page with customizable name

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#task 1
 @app.route("/")
 def home():
     return render_template("index.html")
+
+#task 2
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", user=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
