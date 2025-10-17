2debf4ae95bc66b82012470e43429f1fcc5c59f6
ayumiyua21 <misbhakulridho@gmail.com>
2025-10-17 14:38:21 +0800

    make saving notes persist using json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import json, os
 
 app = Flask(__name__)
 
@@ -45,21 +46,51 @@ def feedback():
         return render_template("feedback.html", respone=message)
     return render_template("feedback.html", respone=None)
 
+#task 10
+def load_notes():
+    if os.path.exists("data.json"):
+        with open("data.json", "r") as f:
+            return json.load(f)
+    return []
+
+def save_notes(notes):
+    with open("data.json", "w") as f:
+        json.dump(notes, f, indent=2)
+
 #task 7
 notes = []
 @app.route("/notes", methods=["GET", "POST"])
 def notes_page():
+    notes = load_notes() #task 10
+
     if request.method == "POST":
         note = request.form["note"]
-        notes.append(note)
+        #notes.append(note)
+        if note.strip():
+            notes.append(note)
+            save_notes(notes)
     return render_template("notes.html", notes=notes)
 
 #task 8
 @app.route("/delete/<int:index>")
 def delete(index):
+    notes = load_notes() #task 10
     if 0 <= index < len(notes):
         notes.pop(index)
+        save_notes(notes) #task 10
     return redirect("/notes")
+
+#task 11
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
+def edit(index):
+    notes = load_notes()
+    if request.method == "POST":
+        new_text = request.form["note"]
+        notes[index] = new_text
+        save_notes(notes)
+        return redirect("/notes")
+    return render_template("edit.html", note=notes[index])
 
 if __name__ == "__main__":
     app.run(debug=True)
+
